055a301a10084890acf06c414bc2a711dc4074cc
HeRc010 <adlawren010@gmail.com>
2016-03-07 17:49:20 -0700

    Added base activity for the UserEditProfile screen, in addition to the default layout for the activity.

---
+++ src/app/src/main/java/ca/ualberta/papaya/UserEditProfile.java
@@ -0,0 +1,20 @@
+package ca.ualberta.papaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UserEditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_edit_profile);
+
+        EditText userInfoEditText; // = (EditText) findViewById(R.id. ...);
+
+        Button saveButton; // = (Button) findViewById(R.id. ...);
+        Button cancelButton; // = (Button) findViewById(R.id. ...);
+    }
+}
