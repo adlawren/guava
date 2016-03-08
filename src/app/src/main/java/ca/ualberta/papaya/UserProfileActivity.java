7df4c0268050000cf474f23cf7bb0557ccb4ad41
HeRc010 <adlawren010@gmail.com>
2016-03-08 11:37:12 -0700

    Added stub UI components to the UserProfile Activity, and associated logic for populating the views within the Activity using a currently 'null' intent.

--- src/app/src/main/java/ca/ualberta/papaya/UserProfileActivity.java
+++ src/app/src/main/java/ca/ualberta/papaya/UserProfileActivity.java
@@ -1,7 +1,11 @@
 package ca.ualberta.papaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import ca.ualberta.papaya.models.User;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        Intent intent = getIntent();
+        User user = (User) intent.getSerializableExtra(""); // TODO: establish public identifier
+
+        TextView userNameTextView = (TextView) findViewById(R.id.user_profile_user_name);
+        userNameTextView.setText(user.getName());
+
+        TextView userEmailTextView = (TextView) findViewById(R.id.user_profile_user_email);
+        userNameTextView.setText(user.getEmail());
+
+        TextView userAddress1TextView = (TextView) findViewById(R.id.user_profile_user_address1);
+        userNameTextView.setText(user.getAddress1());
+
+        TextView userAddress2TextView = (TextView) findViewById(R.id.user_profile_user_address2);
+        userNameTextView.setText(user.getAddress2());
+
+        TextView userProvinceTextView = (TextView) findViewById(R.id.user_profile_user_province);
+        userNameTextView.setText(user.getProvince().toString());
+
+        TextView userCountryTextView = (TextView) findViewById(R.id.user_profile_user_country);
+        userNameTextView.setText(user.getCountry().toString());
+
+        TextView userPostalTextView = (TextView) findViewById(R.id.user_profile_user_postal);
+        userNameTextView.setText(user.getPostal());
     }
 }
