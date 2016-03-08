686e53784552ad4b0ea5f6611fb9dbaa5d833e1f
HeRc010 <adlawren010@gmail.com>
2016-03-07 18:45:42 -0700

    Added stub UI components.

--- src/app/src/main/java/ca/ualberta/papaya/UserEditProfile.java
+++ src/app/src/main/java/ca/ualberta/papaya/UserEditProfile.java
@@ -2,6 +2,7 @@ package ca.ualberta.papaya;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,9 +13,22 @@ public class UserEditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_edit_profile);
 
-        EditText userInfoEditText; // = (EditText) findViewById(R.id. ...);
+        EditText userInfoEditText = (EditText) findViewById(R.id.edit_user_profile_edit_contact_information);
 
-        Button saveButton; // = (Button) findViewById(R.id. ...);
-        Button cancelButton; // = (Button) findViewById(R.id. ...);
+        Button saveButton = (Button) findViewById(R.id.edit_user_profile_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button cancelButton = (Button) findViewById(R.id.edit_user_profile_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
