2dcfbac64cb7f75151de433f06bfc8a49ebb8e1c
hsbarker <hsbarker@ualberta.ca>
2016-03-08 21:53:51 -0700

    Created all UIs minus a couple features

--- src/app/src/main/java/ca/ualberta/papaya/ThingListActivity.java
+++ src/app/src/main/java/ca/ualberta/papaya/ThingListActivity.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v7.app.ActionBarActivity;
 
 
 import ca.ualberta.papaya.dummy.DummyContent;
@@ -43,16 +44,8 @@ public class ThingListActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
+        //toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         View recyclerView = findViewById(R.id.thing_list);
         assert recyclerView != null;
@@ -66,6 +59,7 @@ public class ThingListActivity extends AppCompatActivity {
             mTwoPane = true;
         }
     }
+
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
