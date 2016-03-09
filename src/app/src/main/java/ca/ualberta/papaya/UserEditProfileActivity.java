367ebef33d78b2beb63ecec8f6d2ec48492a26d4
HeRc010 <adlawren010@gmail.com>
2016-03-08 21:50:33 -0700

    Updated test cases to ensure successful compilation, some functionality remains to be added in order for some test cases to be implemented as designed.

--- src/app/src/main/java/ca/ualberta/papaya/UserEditProfileActivity.java
+++ src/app/src/main/java/ca/ualberta/papaya/UserEditProfileActivity.java
@@ -1,6 +1,5 @@
 package ca.ualberta.papaya;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
--- src/app/src/main/java/ca/ualberta/papaya/fixtures/Province.java
+++ src/app/src/main/java/ca/ualberta/papaya/fixtures/Province.java
@@ -4,7 +4,16 @@ package ca.ualberta.papaya.fixtures;
  * Created by martin on 10/02/16.
  */
 public enum Province {
-    ALBERTA, SASKATCHEWAN;
+    BRITISH_COLUMBIA,
+    ALBERTA,
+    SASKATCHEWAN,
+    MANITOBA,
+    ONTARIO,
+    QUEBEC,
+    NOVA_SCOTIA,
+    NEW_BRUNSWICK,
+    PRINCE_EDWARD_ISLAND,
+    NEWFOUNDLAND;
     @Override public String toString() {
         // todo: deal with _
         return name().toLowerCase();
